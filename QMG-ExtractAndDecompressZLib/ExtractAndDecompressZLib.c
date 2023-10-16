d23cbf904c06b0edb73fd6b9418219b5fd1a2acc
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-10-16 08:52:45 -0700

    e

--- QMG-ExtractAndDecompressZLib/ExtractAndDecompressZLib.c
+++ QMG-ExtractAndDecompressZLib/ExtractAndDecompressZLib.c
@@ -3,14 +3,25 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool runZlibFlate(const char *inputFile, const char *outputFile) {
+// Dexrn -----
+// TODO: Maybe make it output to a folder instead, maybe as a setting.
+// TODO(maybe): Add more header types, if there are more found... which means I have to learn how to make it check for multiple, maybe ill pull a funny on line 69 (nice)
+// also I had to remove/change some of the code comments as maybe having the ai try to add code comments is a horrible thing
+// Someone says this code looks like trash when formatted by the AI, but they don't know what it used to look like :tf:
+// I hate the variable name newFilename
+// Maybe one day I'll get better at C/C++
+
+
+bool FlateIt(const char *inputFile, const char *outputFile) {
     char command[256];
+    // this runs the zlib-flate because for some reason it is the only thing that will actually flate' it, I've tried python to no avail lmao.
     snprintf(command, sizeof(command), "zlib-flate -uncompress < %s > %s", inputFile, outputFile);
 
     int status = system(command);
 
     if (status != 0) {
-        fprintf(stderr, "Error running zlib-flate on %s\n", inputFile);
+        // This is to be expected, unless we can find a fix for some files not being valid.
+        fprintf(stderr, "zlib-flate encountered an error with file %s\n", inputFile);
         return false;
     }
 
@@ -22,12 +33,12 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s <input_filename>\n", argv[0]);
         return 1;
     }
-
+    // Was inputFile really a good name? I had the ai help with something and it created that so I just stuck with it.
     const char *inputFilename = argv[1];
     FILE *inputFile = fopen(inputFilename, "rb");
 
     if (inputFile == NULL) {
-        perror("Error opening input file");
+        perror("Error opening file");
         return 2;
     }
 
@@ -35,7 +46,6 @@ int main(int argc, char *argv[]) {
     long fileSize = ftell(inputFile);
     rewind(inputFile);
 
-    // Read the entire file into memory
     unsigned char *fileBuffer = (unsigned char *)malloc(fileSize);
     if (fileBuffer == NULL) {
         perror("Error allocating memory for file buffer");
@@ -53,18 +63,18 @@ int main(int argc, char *argv[]) {
 
     fclose(inputFile);
 
-    // Search for ZLib headers (78 DA)
+    // Look for the header (78 DA), which is the header of interest here as QMG uses it for ZLib.
     int headerCount = 0;
     for (long i = 0; i < fileSize - 1; i++) {
         if (fileBuffer[i] == 0x78 && fileBuffer[i + 1] == 0xDA) {
             headerCount++;
-            // Create a new output file for each header
+            // this should cut out and create a file for every header found, seems like it works.
             char newFilename[strlen(inputFilename) + 16];
             snprintf(newFilename, sizeof(newFilename), "%s-zlib%d", inputFilename, headerCount);
 
             FILE *outputFile = fopen(newFilename, "wb");
             if (outputFile == NULL) {
-                perror("Error creating output file");
+                perror("Error creating file");
                 free(fileBuffer);
                 return 5;
             }
@@ -72,22 +82,20 @@ int main(int argc, char *argv[]) {
             fwrite(fileBuffer + i, 1, fileSize - i, outputFile);
             fclose(outputFile);
 
-            char uncompressedFilename[strlen(newFilename) + 13];
-            snprintf(uncompressedFilename, sizeof(uncompressedFilename), "%s-uncompressed", newFilename);
-
-            // Try running zlib-flate and continue on failure
-            if (!runZlibFlate(newFilename, uncompressedFilename)) {
-                // Handle the error here, if needed
+            char flateFilename[strlen(newFilename) + 13];
+            snprintf(flateFilename, sizeof(flateFilename), "%s-flate", newFilename);
+            if (!FlateIt(newFilename, flateFilename)) {
             }
 
-            printf("Extracted and uncompressed data to %s\n", uncompressedFilename);
+            printf("Extracted + flate'd data to %s\n", flateFilename);
         }
     }
 
     if (headerCount == 0) {
-        printf("No ZLib headers found in the file.\n");
+        // Maybe we should add more header types?
+        printf("No ZLib headers found.\n");
     } else {
-        printf("Found and saved %d ZLib headers to separate files.\n", headerCount);
+        printf("Found %d ZLib headers.\n", headerCount);
     }
 
     free(fileBuffer);
