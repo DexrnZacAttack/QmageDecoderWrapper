91de56d5c5e9c2dd5a7d3855f837b4521cf1bafc
vlOd <66838724+vlOd2@users.noreply.github.com>
2023-10-20 11:02:01 +0300

    [CSharpQmageDecoder] First commit

---
+++ Tools/CSharpQmageDecoder/CSharpQmageDecoder/CSharpQmageDecoder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpQmageDecoderNS
+{
+    class CSharpQmageDecoder
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream file = new FileStream(args[0], FileMode.Open);
+                byte[] headerRaw = new byte[12];
+                file.Read(headerRaw, 0, 12);
+
+                QmageHeader header = new QmageHeader(headerRaw);
+                Console.WriteLine($"Qmage File v{header.VerMajor}.{header.VerMinor}.{header.VerRevision}");
+                Console.WriteLine($"- Flags: {Convert.ToString(header.Flags, 2).PadLeft(8, '0')}");
+                Console.WriteLine($"- Quality: {header.Quality}%");
+                Console.WriteLine($"- Resolution: {header.Width}x{header.Height}");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Tools/CSharpQmageDecoder/CSharpQmageDecoder/QmageHeader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpQmageDecoderNS
+{
+    public class QmageHeader
+    {
+        public byte VerMajor;
+        public byte VerMinor;
+        public byte VerRevision;
+        public byte Flags;
+        public byte Quality;
+        public short Width;
+        public short Height;
+
+        public QmageHeader(byte[] headerRaw)
+        {
+            if (headerRaw.Length < 12 || headerRaw.Length > 12)
+                throw new ArgumentException("Invalid header size!");
+
+            byte[] headerName = headerRaw.Take(2).ToArray();
+            if (headerName[0] != 'Q' || headerName[1] != 'G')
+                throw new ArgumentException("Bad header name! Only QG is supported!");
+
+            byte[] versionRaw = headerRaw.Skip(2).Take(3).ToArray();
+            VerMajor = versionRaw[0];
+            VerMinor = versionRaw[1];
+            VerRevision = versionRaw[2];
+            Flags = headerRaw.Skip(5).Take(1).ToArray()[0];
+            Quality = headerRaw.Skip(6).Take(1).ToArray()[0];
+            Width = BitConverter.ToInt16(headerRaw.Skip(7).Take(2).ToArray(), 0);
+            Height = BitConverter.ToInt16(headerRaw.Skip(9).Take(2).ToArray(), 0);
+        }
+    }
+}
