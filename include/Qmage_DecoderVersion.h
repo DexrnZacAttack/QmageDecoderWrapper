f7ec880fbed919792e193aebedf8e138ab602ef6
Ned Loynd <ned@loynd.com>
2023-10-29 11:12:14 +1100

    Enum value fixes

--- include/Qmage_DecoderVersion.h
+++ include/Qmage_DecoderVersion.h
@@ -1,5 +1,5 @@
 enum Qmage_DecoderVersion {
-    QM_DEC_QMAGE_VER_1_43_LESS,
-    QM_DEC_QMAGE_VER_1_43_MORE,
-    QM_DEC_QMAGE_VER_1_70
+    QM_DEC_QMAGE_VER_1_43_LESS = 11,
+    QM_DEC_QMAGE_VER_1_43_MORE = 12,
+    QM_DEC_QMAGE_VER_1_70 = 13
 };
